0a3d03fa5c807da0e930564f698dcea34fdeaedb
Juan Hernandez <js.hernandez15@uniandes.edu.co>
2019-03-18 12:38:09 -0500

    main class!

--- model/hadoop.js
+++ model/hadoop.js
@@ -32,7 +32,7 @@ command.readRF3 = function(id, callback) {
 command.sendRA1 = function(id, data) {
     var SSH = new SSH2Shell({
         server: this.server,
-        commands: ["hadoop jar /home/bigdata04/taller1/ra1v5.jar datos/taxis_subsample output/taller1/RA1_V5_" + id.replace('-', '_') + " -1 -1 0 -1 -1 -1"]
+        commands: ["hadoop jar /home/bigdata04/taller1/ra1v5.jar job.RA1 datos/taxis_subsample output/taller1/RA1_V5_" + id.replace('-', '_') + " -1 -1 0 -1 -1 -1"]
         //commands: ["echo $(pwd)", "ls -l"]
     });
     var callback = function(sessionText) {
