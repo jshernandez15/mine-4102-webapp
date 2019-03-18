7fba79bb04fea9be2030dfe9d48123ed5b063b5b
Juan Hernandez <js.hernandez15@uniandes.edu.co>
2019-03-17 21:47:02 -0500

    underscore

--- model/hadoop.js
+++ model/hadoop.js
@@ -11,7 +11,7 @@ var command = {
 command.send = function(id, data) {
     var SSH = new SSH2Shell({
         server: this.server,
-        commands: ["hadoop jar /home/bigdata04/taller1/RF3_2.jar uniandes.job.Job3 datos/taxis_subsample output/taller1/RF3_" + id + " 2 10 14 20"]
+        commands: ["hadoop jar /home/bigdata04/taller1/RF3_2.jar uniandes.job.Job3 datos/taxis_subsample output/taller1/RF3_" + id.replace('-', '_') + " 2 10 14 20"]
         //commands: ["echo $(pwd)", "ls -l"]
     });
     var callback = function(sessionText) {
@@ -23,7 +23,7 @@ command.send = function(id, data) {
 command.read = function(id, callback) {
     var SSH = new SSH2Shell({
         server: this.server,
-        commands: ["hadoop fs -cat output/taller1/RF3_" + id + "_ordenados/part-r-00000"]
+        commands: ["hadoop fs -cat output/taller1/RF3_" + id.replace('-', '_') + "_ordenados/part-r-00000"]
         //commands: ["cat mine-4102/mine-4102-webapp/.gitignore"]
     });
     SSH.connect(callback);
